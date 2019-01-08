625821a35658910d0f55d7c6d4fd70467d67e64f
github username <github email address>
2019-01-08 17:46:12 -0500

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,20 +2,26 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
+def get_character_movies_from_api(character_name, link = "http://www.swapi.co/api/people/")
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_string = RestClient.get(link)
   response_hash = JSON.parse(response_string)
 
   # arr =[]
+  arr = []
  response_hash["results"].each do |post|
    # binding.pry
-   if post["name"] == character_name
-      post["films"].map do |link|
-      print_movies(link)
-      end
+     if post["name"] == character_name
+        post["films"].each do |links|
+          arr << print_movies(links)
+        end
+        puts arr
+        return arr
      end
+
    end
+   get_character_movies_from_api(character_name, response_hash["next"])
+ end
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -25,13 +31,12 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
     response_string = RestClient.get(films)
     response_hash = JSON.parse(response_string)
-    p response_hash["title"]
+    response_hash["title"]
 end
 
 # def show_character_movies(character)
